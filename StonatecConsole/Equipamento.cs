91d6372cd78532ab6f58443e276d3ba59c88c912
Feranando Stonoga <foxtrotechoromeu@gmail.com>
2020-08-25 00:25:49 -0300

    criada classe equipamento

---
+++ StonatecConsole/Equipamento.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StonatecConsole
+{
+    class Equipamento
+    {
+        public string Equipamentos { get; set; }
+        public string Modelo { get; set; }
+    }
+}
--- StonatecConsole/Program.cs
+++ StonatecConsole/Program.cs
@@ -7,15 +7,44 @@ namespace StonatecConsole
         static void Main(string[] args)
         {
             int opcao;
+            Equipamento e = new Equipamento();
             do
             {
                 Console.Clear();
                 Console.WriteLine("---Stonatec sistemas--- \n");
-                Console.WriteLine("1 Cadastrar equipamentos");
-                Console.WriteLine("2 Listar equipamentos");
+                Console.WriteLine("1 Cadastrar Equipamentos");
+                Console.WriteLine("2 Listar Equipamentos");
                 Console.WriteLine("0 Sair");
                 Console.WriteLine("Digite a opção desejada: \n");
                 opcao = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Cadastrar Equipamentos \n");
+                        Console.WriteLine("Digite o nome do Equipamento: \n");
+                        e.Equipamentos = Console.ReadLine();
+                        Console.WriteLine("Digite o modelo do equipamento: \n");
+                        e.Modelo = Console.ReadLine();
+                        Console.WriteLine($"Equipamento: {e.Equipamentos} e modelo: {e.Modelo}\n");
+
+
+
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Listar Equipamentos \n");
+                        break;
+                    case 3:
+                        Console.WriteLine("Saindo... \n");
+                        break;
+                    default:
+                        Console.WriteLine("Opção Inválida... \n");
+                        break;
+                }
+                Console.WriteLine("\nPressione uma tecla para continuar...\n");
+                Console.ReadKey();
+
             } while (opcao != 0);
         }
     }
