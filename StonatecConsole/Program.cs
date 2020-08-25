3815b78bb2c132a6e690cc3c6f0e21ba4f9534ad
Feranando Stonoga <foxtrotechoromeu@gmail.com>
2020-08-24 23:15:57 -0300

    adicionar o menu a aplicação

--- StonatecConsole/Program.cs
+++ StonatecConsole/Program.cs
@@ -6,7 +6,17 @@ namespace StonatecConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int opcao;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("---Stonatec sistemas--- \n");
+                Console.WriteLine("1 Cadastrar equipamentos");
+                Console.WriteLine("2 Listar equipamentos");
+                Console.WriteLine("0 Sair");
+                Console.WriteLine("Digite a opção desejada: \n");
+                opcao = Convert.ToInt32(Console.ReadLine());
+            } while (opcao != 0);
         }
     }
 }
