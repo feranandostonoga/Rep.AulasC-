34f194412038eda8fc870889ebf0be6b6a1c4b60
Feranando Stonoga <foxtrotechoromeu@gmail.com>
2020-08-28 21:12:34 -0300

    Modificação do sistema para vendas

---
+++ StonatecConsole/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StonatecConsole
+{
+    class Cliente
+    {
+        public Cliente()
+        {
+            CriadoEm = DateTime.Now;
+                
+        }
+        public string Clientes { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime CriadoEm { get; set; }
+
+        public override string ToString()
+        {
+            return $"equipamento: {Clientes} | Modelo: {Nome} |  Serial Number: {Cpf} | Criado em: {CriadoEm}";
+
+
+        }
+
+    }
+}
--- StonatecConsole/Program.cs
+++ StonatecConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StonatecConsole
 {
@@ -7,33 +8,64 @@ namespace StonatecConsole
         static void Main(string[] args)
         {
             int opcao;
-            Equipamento e = new Equipamento();
+            Cliente c = new Cliente();
+            List<Cliente> clientes = new List<Cliente>();
             do
             {
                 Console.Clear();
                 Console.WriteLine("---Stonatec sistemas--- \n");
-                Console.WriteLine("1 Cadastrar Equipamentos");
-                Console.WriteLine("2 Listar Equipamentos");
+                Console.WriteLine("1 Cadastrar cliente");
+                Console.WriteLine("2 Listar cliente");
                 Console.WriteLine("0 Sair");
                 Console.WriteLine("Digite a opção desejada: \n");
-                opcao = Convert.ToInt32(Console.ReadLine());
+
+                int v = Convert.ToInt32(Console.ReadLine());
+                opcao = v;
                 Console.Clear();
+
                 switch (opcao)
                 {
                     case 1:
-                        Console.WriteLine("Cadastrar Equipamentos \n");
-                        Console.WriteLine("Digite o nome do Equipamento: \n");
-                        e.Equipamentos = Console.ReadLine();
-                        Console.WriteLine("Digite o modelo do equipamento: \n");
-                        e.Modelo = Console.ReadLine();
-                        Console.WriteLine($"Equipamento: {e.Equipamentos} e modelo: {e.Modelo}\n");
-
-
-
-
+                        c = new Cliente();
+                        Console.WriteLine("Cadastrar cliente \n");
+                        Console.WriteLine("Digite o nome: \n");
+                        c.Nome = Console.ReadLine();
+                        Console.WriteLine("Digite o cpf: \n");
+                        c.Cpf = Console.ReadLine();
+                       
+                        if (clientes.Count == 0)
+                        {
+                            clientes.Add(c);
+                            Console.WriteLine("Cliente salvo com sucesso...");
+                        }
+                        else
+                        {
+                            bool encontrado = false;
+                            foreach (Cliente clienteCadastrado in clientes)
+                            {
+                                if (clienteCadastrado.Cpf == c.Cpf)
+                                {
+                                    encontrado = true;
+                                }
+                            }
+                            if (!encontrado)
+                            {
+                                clientes.Add(c);
+                                Console.WriteLine("Cliente salvo com sucesso!!!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Não foi possível adicionar.");
+                            }
+                        }
                         break;
                     case 2:
                         Console.WriteLine("Listar Equipamentos \n");
+                        foreach (Cliente clienteCadastrado in clientes)
+                        {
+                            Console.WriteLine(clienteCadastrado);
+                        }
+                        Console.ReadKey();
                         break;
                     case 3:
                         Console.WriteLine("Saindo... \n");
@@ -42,6 +74,7 @@ namespace StonatecConsole
                         Console.WriteLine("Opção Inválida... \n");
                         break;
                 }
+               
                 Console.WriteLine("\nPressione uma tecla para continuar...\n");
                 Console.ReadKey();
 
